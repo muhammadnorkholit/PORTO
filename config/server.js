bf6e14620ce1326aa4b5edddfe6d451217b7238e
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2023-08-17 17:33:03 +0700

    deploy strapi

---
+++ config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "192.168.1.4"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+});
